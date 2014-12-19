ba7b3d30725593bb21a1c1ac8c3baad11e2e0eb0
aleksandar-todorovic <aleksandar.todorovic@mail.ru>
2014-12-19 14:23:17 +0100

    Hello World 1

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,10 @@
+// helloworld.cpp by Aleksandar Todorović
+// Description: A simple Hello World program to begin with.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    return 0;
+}
